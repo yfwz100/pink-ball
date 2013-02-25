e59d1c3621ec9a547e2140a9b09d898db9fce059
yfwz100 <yfwz100@yeah.net>
2013-02-25 16:10:34 +0800

    Change the implementation of selectable.js to support the data attributes in HTML 5 more friendly.

--- 224/selectable.js
+++ 224/selectable.js
@@ -3,43 +3,19 @@
 (function () {
   "use strict"
 
-  function getTop(e) {
-    var offset = e.offsetTop;
-    if (e.offsetParent != null) offset += getTop(e.offsetParent);
-    return offset;
-  };
-
-  function getLeft(e) {
-    var offset = e.offsetLeft;
-    if (e.offsetParent != null) offset += getLeft(e.offsetParent);
-    return offset;
-  };
-
-  function makeSelectableImpl(elem) {
-    document.addEventListener('click', function (event) {
-      var dx = event.clientX - getLeft(elem);
-      var dy = event.clientY - getTop(elem);
-      if (dx > 0 && dx < elem.offsetWidth && dy > 0 && dy < elem.offsetHeight) {
-        elem.dataset.selected = true;
-      }
-    });
-  }
-
-  function makeSelectable(e) {
-    if (typeof e == 'string') {
-      var elems = document.querySelectorAll(e);
-      for (var i=0; i<elems.length; i++) {
-        makeSelectableImpl(elems[i]);
-      }
-    } else {
-      makeSelectableImpl(e);
-    }
-  }
-
-  window.makeSelectable = makeSelectable;
-
   function onLoad() {
-    makeSelectable('[data-selectable="true"]');
+    document.addEventListener('click', function (event) {
+      var elems = document.querySelectorAll('[data-selectable="true"]');
+      for (var i=0; i<elems.length; i++) {
+        elems[i].dataset.selected = false;
+      }
+      var target = event.target || {parent:null,dataset:{selectable:false}};
+      do {
+        if (target.dataset && target.dataset.selectable == "true") {
+          target.dataset.selected = "true";
+        }
+      } while ((target=target.parentNode) != null);
+    });
   };
 
   window.onload = window.onload ? (function (func) {
